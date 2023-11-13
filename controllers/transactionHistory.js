72550daf3fc5476aeecaf8c00d705277cc20af76
dkbgroupcompany <139337270+dkbgroupcompany@users.noreply.github.com>
2023-11-13 04:56:50 -0800

    Create transactionHistory.js

---
+++ controllers/transactionHistory.js
@@ -0,0 +1,17 @@
+const Transaction = require('../model/Transactions'); 
+const User = require("../model/Users");
+
+
+const transactionHistory = async (req, res)=>{
+
+    const cookies = req.cookies;
+    if (!cookies?.jwt) return res.sendStatus(401);
+    const refreshToken = cookies.jwt;
+    const foundUser = await User.findOne({ refreshToken }).exec();
+    const userId = foundUser._id;
+    const transactions = await Transaction.find({ user:userId }).sort({transactionDate: -1}); 
+    res.json(transactions)
+}
+
+
+module.exports=transactionHistory
