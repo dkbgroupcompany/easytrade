b101ea9dbfc5c2746ba91204caf3fa85705582f9
dkbgroupcompany <139337270+dkbgroupcompany@users.noreply.github.com>
2023-11-13 04:25:52 -0800

    Create findb.js

---
+++ controllers/findb.js
@@ -0,0 +1,17 @@
+// const paystack = require("paystack")(`$ ${process.env.PAYSTACK_SECRETY_CODE}`);
+
+// // Retrieve the balance of a customer
+// paystack.balance
+//   .get({
+//     email: "ahmadayyub200@gmail.com",
+//   })
+//   .then((response) => {
+//     // The response contains an array of balances for each currency
+//     console.log(response.data);
+//   })
+//   .catch((error) => {
+//     // Handle any error
+//     console.error(error);
+//   });
+
+//   module.exports = paystack;
